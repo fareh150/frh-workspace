ba4ff5da9d47809657dd24ac402d0bc2bfbd398d
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-22 23:56:37 +0100

    wip

--- projects/frh-side-menu/src/lib/frh-side-menu.component.ts
+++ projects/frh-side-menu/src/lib/frh-side-menu.component.ts
@@ -20,6 +20,8 @@ export enum TitleColor{
 })
 export class FrhSideMenuComponent
 {
+  title = input('FRH')
+  subtitle = input('Corp')
   titleColor = input<TitleColor>(TitleColor.purple);
 
   activeCss = "rounded-lg bg-gradient-to-tr from-blue-600 to-blue-400 text-white shadow-md shadow-blue-500/20 active:opacity-[0.85] w-full flex items-center gap-4 px-4 capitalize"
