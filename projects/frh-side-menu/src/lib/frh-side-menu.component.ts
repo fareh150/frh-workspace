23a34e2ccf991060b9896bad29ddc02f8e0180f5
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-22 14:53:34 +0100

    add TitleColor to the component

--- projects/frh-side-menu/src/lib/frh-side-menu.component.ts
+++ projects/frh-side-menu/src/lib/frh-side-menu.component.ts
@@ -1,6 +1,13 @@
 import { Component, input, output } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
+export enum TitleColor{
+  red = 'text-red-500',
+  green = 'text-green-500',
+  blue = 'text-blue-500',
+  purple = 'text-purple-500',
+}
+
 @Component({
   selector: 'lib-frh-side-menu',
   standalone: true,
@@ -13,6 +20,8 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class FrhSideMenuComponent
 {
+  titleColor = input<TitleColor>(TitleColor.purple);
+
   activeCss = "rounded-lg bg-gradient-to-tr from-blue-600 to-blue-400 text-white shadow-md shadow-blue-500/20 active:opacity-[0.85] w-full flex items-center gap-4 px-4 capitalize"
   isAuthenticated = input(false);
   onSignOut = output();
